59e6474ce757174154a7cc3f79d1eb621861106c
Marshall Bowers <elliott.codes@gmail.com>
2022-07-13 17:16:07 -0400

    Scaffold out the various log events

--- src/pnpm_log_line.rs
+++ src/pnpm_log_line.rs
@@ -3,7 +3,71 @@ use chrono::{DateTime, Utc};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct PnpmLogLine {
     #[serde(with = "ts_milliseconds")]
     pub time: DateTime<Utc>,
+
+    pub hostname: String,
+
+    pub pid: i32,
+
+    pub level: LogLevel,
+
+    #[serde(flatten)]
+    pub event: PnpmLogEvent,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "name")]
+pub enum PnpmLogEvent {
+    #[serde(rename = "pnpm")]
+    Message {
+        message: String,
+        prefix: Option<String>,
+    },
+
+    #[serde(rename = "pnpm:global")]
+    Global { message: String },
+
+    #[serde(rename = "pnpm:scope")]
+    Scope { selected: i32 },
+
+    #[serde(rename = "pnpm:_dependency_resolved")]
+    DependencyResolved,
+
+    #[serde(rename = "pnpm:progress")]
+    Progress,
+
+    #[serde(rename = "pnpm:fetching-progress")]
+    FetchingProgress,
+
+    #[serde(rename = "pnpm:install-check")]
+    InstallCheck,
+
+    #[serde(rename = "pnpm:skipped-optional-dependency")]
+    SkippedOptionalDependency,
+
+    #[serde(rename = "pnpm:link")]
+    Link,
+
+    #[serde(rename = "pnpm:lifecycle")]
+    Lifecycle,
+
+    #[serde(rename = "pnpm:lockfile")]
+    Lockfile,
+
+    #[serde(rename = "pnpm:summary")]
+    Summary,
+
+    #[serde(other)]
+    Unknown,
 }
